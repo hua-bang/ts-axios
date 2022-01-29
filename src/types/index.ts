3febfa9fec3f5c6594edfa2184f8c2047563b266
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-01-29 22:26:03 +0800

    feat(axios base): axios base code

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type RequestMethod =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: RequestMethod
+  data?: any
+  params?: any
+}
